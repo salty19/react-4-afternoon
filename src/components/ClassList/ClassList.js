7c19aa166f3b616073b13c0b2d1d31f8cfb7fa71
Kara LeSueur <kara.lesueur@gmail.com>
2020-09-03 15:50:53 -0700

    end of 5

--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class ClassList extends Component {
   constructor() {
     super()
     
+    this.state = {
+      students: [],
+    }
   }
+
+  componentDidMount() {
+
+    axios.get(`http://localhost:3005/students?class=${this.props.match.params.class}`)
+    .then(results => {
+      this.setState({
+        students: results.data
+      })
+    })
+  }
+
+
 
   render() {
     return (
